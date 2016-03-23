e16746b0d10bff5a9c5145568967087cdd4e2453
jafoole <smallbabba@gmail.com>
2016-03-23 15:23:11 -0400

    Added User - Tare, Greg, Adi, Simon

--- FirebaseLab/app/src/main/java/com/example/jafoole/firebaselab/MainActivity.java
+++ FirebaseLab/app/src/main/java/com/example/jafoole/firebaselab/MainActivity.java
@@ -9,10 +9,16 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 import com.firebase.ui.FirebaseListAdapter;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
 
     Firebase mFirebaseRootRef;
 
+    String mUsername;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,20 +42,27 @@ public class MainActivity extends AppCompatActivity {
         mEditText = (EditText)findViewById(R.id.chatEditText);
         mButton = (Button) findViewById(R.id.sendButton);
 
+
+
         mMessagesList = new ArrayList<>();
 
         mFirebaseRootRef = new Firebase("https://tax-on-fire.firebaseio.com");
 
-//        final Firebase firebaseCurrentTextRef = mFirebaseRootRef.child("current-text");
-        final Firebase firebaseMessageRef = mFirebaseRootRef.child("message");
+//        final Firebase username = mFirebaseRootRef.child("username");
+        final Firebase firebaseMessageRef = mFirebaseRootRef.child("messages");
+        final Firebase username = firebaseMessageRef.child("username");
+        final Firebase message = username.child("message");
 
-        FirebaseListAdapter<String> adapter = new FirebaseListAdapter<String>(this, String.class, android.R.layout.simple_list_item_1, firebaseMessageRef) {
+        final FirebaseListAdapter<String> adapter = new FirebaseListAdapter<String>(this, String.class, android.R.layout.simple_list_item_1, firebaseMessageRef) {
             @Override
             protected void populateView(View view, String s, int i) {
+                String user="User:";g
                 TextView textView = (TextView)view.findViewById(android.R.id.text1);
-                textView.setText(s);
+                textView.setText(user + new Random(10).nextInt() + " \n" + s);
+
             }
         };
+
 
         mListView.setAdapter(adapter);
 
@@ -58,6 +73,35 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+//        firebaseMessageRef.addChildEventListener(new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//                String userName = dataSnapshot.getValue(String.class);
+//                mMessagesList.add(userName);
+//                adapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+//
+//            }
+//
+//            @Override
+//            public void onChildRemoved(DataSnapshot dataSnapshot) {
+//
+//            }
+//
+//            @Override
+//            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(FirebaseError firebaseError) {
+//
+//            }
+//        });
 
 
 
