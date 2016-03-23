f7c0f92de3821dbb67319767e64ff4a25bbbf6ab
jafoole <smallbabba@gmail.com>
2016-03-23 13:55:02 -0400

    Firebase lab

---
+++ FirebaseLab/app/src/main/java/com/example/jafoole/firebaselab/MyApplication.java
@@ -0,0 +1,22 @@
+package com.example.jafoole.firebaselab;
+
+import android.app.Application;
+
+/**
+ * Created by
+ * /\     /\      __        _____              __                /\      /\
+ * \ \    \ \     |__|____ _/ ____\____   ____ |  |   ____       / /     / /
+ * \ \    \ \     |  \__  \\   __\/  _ \ /  _ \|  | _/ __ \     / /     / /
+ * \ \    \ \     |  |/ __ \|  | (  <_> |  <_> )  |_\  ___/    / /     / /
+ * \ \    \ \ /\__|  (____  /__|  \____/ \____/|____/\___  >  / /     / /
+ * \/     \/  \______|    \/                             \/   \/      \/
+ * on 3/23/16.
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
